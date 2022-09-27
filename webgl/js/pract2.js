f429705cc338a1a63930abff87fe8461d6e44c58
Dani2383 <danieldc1000@gmail.com>
2022-09-27 19:13:47 +0200

    Update pract2.js

--- webgl/js/pract2.js
+++ webgl/js/pract2.js
@@ -13,7 +13,7 @@ render();
 
 function init(){
     renderer = new Three.WebGLRenderer();
-    console.log('Hola12'); 
+    console.log('Hola13'); 
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.getElementById('container').appendChild(renderer.domElement);
 
