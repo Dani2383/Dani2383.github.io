bae8ebb81b0fea9ecea739253016d46574ba8020
Dani2383 <danieldc1000@gmail.com>
2022-10-05 17:12:03 +0200

    pract4

--- webgl/js/AnimatedScene.js
+++ webgl/js/AnimatedScene.js
@@ -1,5 +1,5 @@
 /**
- * EscenaMultivista.js
+ * AnimatedScene.js
  * 
  * Seminario GPC#3. Visualizar una escena básica desde 4 camaras
  * en 4 marcos diferentes.
@@ -12,15 +12,17 @@
 import * as THREE from "../lib/three.module.js";
 import {GLTFLoader} from "../lib/GLTFLoader.module.js";
 import {OrbitControls} from "../lib/OrbitControls.module.js";
+import {TWEEN } from "../lib/tween.module.min.js";
+import { GUI } from "../lib/lil-gui.module.min.js";
+import Stats from "../lib/stats.module.js";
 
 // Variables estandar
-let renderer, scene, camera;
+let renderer, scene, camera, esfera, cubo;
 
 // Otras globales
 let esferaCubo;
 let angulo = 0;
 let cameraControls;
-let alzado, planta, perfil;
 const L = 5;
 
 // Acciones
@@ -28,39 +30,12 @@ init();
 loadScene();
 render();
 
-function setCameras(ar)
-{
-    let camaraOrto;
-
-    // Construir las camaras ortograficas
-    if(ar>1)
-     camaraOrto = new THREE.OrthographicCamera(-L*ar,L*ar,L,-L,-10,100);
-    else
-     camaraOrto = new THREE.OrthographicCamera(-L,L,L/ar,-L/ar,-10,100);
-
-    alzado = camaraOrto.clone();
-    alzado.position.set(0,0,10);
-    alzado.lookAt(0,0,0);
-
-    perfil = camaraOrto.clone();
-    perfil.position.set(10,0,0);
-    perfil.lookAt(0,0,0);
-
-    planta = camaraOrto.clone();
-    planta.position.set(0,10,0);
-    planta.lookAt(0,0,0);
-    planta.up = new THREE.Vector3(0,0,-1);
-
-    
-}
-
 function init()
 {
     // Instanciar el motor de render
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth,window.innerHeight);
     renderer.setClearColor(0xAAAAAA);
-    renderer.autoClear = false;
     document.getElementById('container').appendChild( renderer.domElement );
 
     // Instanciar el nodo raiz de la escena
@@ -68,16 +43,12 @@ function init()
     //scene.background = new THREE.Color(0.5,0.5,0.5);
 
     // Instanciar la camara perspectiva
-    const ar = window.innerWidth/window.innerHeight;
     camera= new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,1,100);
     camera.position.set(0.5,2,7);
 
     cameraControls = new OrbitControls(camera,renderer.domElement);
     cameraControls.target.set(0,1,0);
     camera.lookAt(0,1,0);
-
-    // Otras camaras
-    setCameras(ar);
 
     // Captura de eventos
     window.addEventListener('resize', updateAspectRatio );
@@ -97,8 +68,8 @@ function loadScene()
     scene.add(suelo);
 
     // Esfera y cubo
-    const esfera = new THREE.Mesh( new THREE.SphereGeometry(1,20,20), material );
-    const cubo = new THREE.Mesh( new THREE.BoxGeometry(2,2,2), material );
+    esfera = new THREE.Mesh( new THREE.SphereGeometry(1,20,20), material );
+    cubo = new THREE.Mesh( new THREE.BoxGeometry(2,2,2), material );
     esfera.position.x = 1;
     cubo.position.x = -1;
 
@@ -120,6 +91,7 @@ function loadScene()
     {
         cubo.add(objeto);
         objeto.position.y = 1;
+        objeto.scene.name = soldado;
     });
 
     const glloader = new GLTFLoader();
@@ -130,6 +102,7 @@ function loadScene()
         objeto.scene.scale.set(0.5,0.5,0.5);
         objeto.scene.position.y = 1;
         objeto.scene.rotation.y = -Math.PI/2;
+        objeto.scene.name = 'robot';
         console.log("ROBOT");
         console.log(objeto);
     });
@@ -148,72 +121,33 @@ function updateAspectRatio()
     // perspectiva
     camera.aspect = ar;
     camera.updateProjectionMatrix();
-
-    // ortografica
-    if(ar>1){
-        alzado.left = planta.left = perfil.left = -L*ar;
-        alzado.right = planta.right =perfil.right = L*ar;
-    }
-    else{
-        alzado.top = planta.top= perfil.top=  L/ar;
-        alzado.bottom = planta.bottom = perfil.bottom = -L/ar;       
-    }
- 
-
-    alzado.updateProjectionMatrix();
-    perfil.updateProjectionMatrix();
-    planta.updateProjectionMatrix();
 }
 
 function update()
 {
-    angulo += 0.01;
-    //esferaCubo.rotation.y = angulo;
+    TWEEN.update();
 }
 
 function rotateShape(evento)
 {
-    // Capturar la posicion de doble click (S.R. top-left con Y down)
     let x = evento.clientX;
     let y = evento.clientY;
 
-    // Zona de click
-    let derecha = false, abajo = false;
-    let cam = null;
+    x = ( x/window.innerWidth) * 2 -1;
+    y = - ( y/ window.innerHeight ) * 2 + 1;
 
-    if( x > window.innerWidth/2 ){
-        derecha = true;
-        x -= window.innerWidth/2;
-    }
-    if( y > window.innerHeight/2 ){
-        abajo = true;
-        y -= window.innerHeight/2;
-    }
-
-    // x e y estan en el primer cuadrante
-
-    if(derecha)
-        if(abajo) cam = camera;
-        else cam = perfil;
-    else
-        if(abajo) cam = planta;
-        else cam = alzado;
-
-    // cam es la camara que recibe el doble click
-
-    // Normalizar las coordenadas de click al cuadrado de 2x2
-
-    x = ( x * 4/window.innerWidth ) - 1;
-    y = -( y * 4/window.innerHeight ) + 1; //--> Ejercicio !!
-
-    // Rayo e intersecciones
     const rayo = new THREE.Raycaster();
-    rayo.setFromCamera(new THREE.Vector2(x,y), cam);
+    rayo.setFromCamera( new THREE.Vector2(x,y), camera);
 
-    const intersecciones = rayo.intersectObjects( 
-        scene.getObjectByName('grupoEC').children,false );
-    if(intersecciones.length>0)
-        intersecciones[0].object.rotation.y += Math.PI / 8;
+    const soldado = scene.getObjectByName('soldado');
+    let intersecciones = rayo.intersectObjects( soldado.children, true);
+    if (intersecciones.length > 0){
+        new TWEEN.Tween(soldado.position)
+            .to({x: [0,0], y: [1,3], z: [0,0]}, 2000)
+            .interpolation(TWEEN.Interpolation.Bezier)
+            .easing(TWEEN.Easing.Exponential.InOut)
+            .start();
+    }
         
 }
 
@@ -221,19 +155,5 @@ function render()
 {
     requestAnimationFrame(render);
     update();
-
-    renderer.clear();
-
-    // El S.R. del viewport es left-bottom con X right y Y up
-    renderer.setViewport(0,window.innerHeight/2, window.innerWidth/2,window.innerHeight/2);
-    renderer.render(scene,alzado);
-
-    renderer.setViewport(0,0, window.innerWidth/2,window.innerHeight/2);
-    renderer.render(scene,planta);
-
-    renderer.setViewport(window.innerWidth/2,window.innerHeight/2, window.innerWidth/2,window.innerHeight/2);
-    renderer.render(scene,perfil);
-
-    renderer.setViewport(window.innerWidth/2,0,window.innerWidth/2,window.innerHeight/2);
-    renderer.render(scene,camera);
+    renderer.render(scene, camera);
 }
